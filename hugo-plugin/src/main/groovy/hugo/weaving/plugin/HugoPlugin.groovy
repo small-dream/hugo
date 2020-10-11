feeae8a858065a954fb3c0ee56af1569135392fa
Jake Jiang <jake.jiang@planetart.cn>
2020-10-11 10:15:23 +0800

    update gradle

--- hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
+++ hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
@@ -26,10 +26,9 @@ class HugoPlugin implements Plugin<Project> {
     }
 
     project.dependencies {
-      debugCompile 'com.jakewharton.hugo:hugo-runtime:1.2.2-SNAPSHOT'
-      // TODO this should come transitively
+      debugCompile 'com.jakewharton.hugo:hugo-runtime:1.2.2'
       debugCompile 'org.aspectj:aspectjrt:1.8.6'
-      compile 'com.jakewharton.hugo:hugo-annotations:1.2.2-SNAPSHOT'
+      compile 'com.jakewharton.hugo:hugo-annotations:1.2.2'
     }
 
     project.extensions.create('hugo', HugoExtension)
